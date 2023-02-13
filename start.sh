f090cab8f2274c13684b9dd84f5d65b105dc9e0a
zhangjh451 <zhangjh451@midea.com>
2023-02-13 15:20:55 +0800

    update

--- start.sh
+++ start.sh
@@ -3,5 +3,10 @@
 work_dir=$(pwd)
 cd "${work_dir}" || exit 1
 
+pid=$(ps -ef | grep java | grep chatgpt-helper | awk '{print $2}')
+if [[ "${pid}" != "" ]];then
+  kill "${pid}"
+fi
+
 mvn clean package && nohup java -jar chatgpt-helper-start/target/*.jar &> ./log/app.log &
 echo $?
