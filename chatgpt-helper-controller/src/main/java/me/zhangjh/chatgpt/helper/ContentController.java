53ca2376d2c12b70b505b8c422054ddfe210b3ab
zhangjh451 <zhangjh451@midea.com>
2023-02-16 14:14:21 +0800

    默认数据不读数据库

--- chatgpt-helper-controller/src/main/java/me/zhangjh/chatgpt/helper/ContentController.java
+++ chatgpt-helper-controller/src/main/java/me/zhangjh/chatgpt/helper/ContentController.java
@@ -14,7 +14,6 @@ import me.zhangjh.chatgpt.helper.model.TblQuestion;
 import me.zhangjh.chatgpt.helper.request.ChatRequest;
 import me.zhangjh.chatgpt.helper.request.DrawRequest;
 import me.zhangjh.chatgpt.helper.service.TblChatService;
-import me.zhangjh.chatgpt.helper.service.TblDefaultQuestionService;
 import me.zhangjh.chatgpt.helper.service.TblDrawService;
 import me.zhangjh.chatgpt.helper.vo.Response;
 import me.zhangjh.chatgpt.util.HttpClientUtil;
@@ -26,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author njhxzhangjihong@126.com
@@ -41,12 +40,68 @@ public class ContentController {
 
     {
         HttpClientUtil.setRequestConfig(RequestConfig.custom()
-                .setConnectTimeout(5000)
+                .setConnectTimeout(50000)
                 // there may be many content returned, set long socket time out
                 .setSocketTimeout(10 * 60 * 1000)
                 .setConnectionRequestTimeout(5000)
                 .build());
+
+        List<String> questions = Arrays.asList("找对象为什么这么难？",
+                "俄乌冲突什么时候能结束？",
+                "怎么看待詹姆斯获得NBA得分王？",
+                "给我来一点Java八股文？",
+                "给我来几个富有诗意的男孩名字？",
+                "给我来首中晚唐的诗歌？",
+                "以和谐为主题帮我写一段讲话稿？",
+                "什么是斐波拉契数列？",
+                "给我讲几个笑话？",
+                "一句话总结一下《甄嬛传》说了什么？",
+                "推荐几部好看的电影？",
+                "推荐几本值得一看的书？",
+                "如何制作精美的PPT？",
+                "架构师应该掌握什么样的技能？",
+                "如何评价马斯克收购推特的行为？",
+                "普通人如何应对通货膨胀？",
+                "有哪些普通人可以进行的投资理财方式？",
+                "推荐一些中国值得旅游的景点？",
+                "推荐一些欧洲值得旅游的景点？",
+                "周杰伦有哪些专辑？",
+                "怎样学习人工智能？",
+                "如何计算笛卡尔积？",
+                "怎样制作原子弹？",
+                "如何快速学习英语？",
+                "Chatgpt会取代哪些工作吗？",
+                "A股为什么总在3000点徘徊？");
+        for (String question : questions) {
+            TblQuestion tblQuestion = new TblQuestion();
+            tblQuestion.setQuestion(question);
+            defaultQuestions.add(tblQuestion);
+        }
+
+        Map<String, String> draws = new HashMap<>();
+        draws.put("海边散步的情侣 动漫风格 高清画质", "https://wx2.sinaimg.cn/bmiddle/62d95157ly1hb4gl6pkfkj20sg0sg1kx.jpg");
+        draws.put("给流浪地球制作一款精美的科幻风格海报", "https://wx2.sinaimg.cn/bmiddle/62d95157ly1hb4h8cwsjgj20sg0sgb29.jpg");
+        draws.put("一只火红的狐狸在吃紫色的葡萄，油画风格", "https://wx4.sinaimg.cn/bmiddle/62d95157ly1hb4go2zg0qj20sg0sge81.jpg");
+        draws.put("Rainforests inside a glass house,lush trees,vines twing around the shelves,greg rutkowski and james gurney and Thomas kinkade", "https://wx4.sinaimg.cn/bmiddle/62d95157ly1hb4h32kokjj20sg0sgkjl.jpg");
+        draws.put("orbital space station,science and technology,Jonn Berkey,highly detailed,artstation", "https://wx3.sinaimg.cn/bmiddle/62d95157ly1hb4h365x97j20sg0sghdt.jpg");
+        draws.put("great chinese palace above clouds,morning,distance waterfalls flow down the mountians,fantasy,high-definition,super wide angle,artstation", "https://wx2.sinaimg.cn/bmiddle/62d95157ly1hb4h3ait7hj20sg0sg7wh.jpg");
+        draws.put("来一张中国水墨画风格的素描，内容是一名渔夫在船上垂钓", "https://wx1.sinaimg.cn/bmiddle/62d95157ly1hb4h3cikthj20sg0sg4qp.jpg");
+        draws.put("可爱的黄色小猫咪", "https://wx1.sinaimg.cn/bmiddle/62d95157ly1hb4hushdpej20sg0sg4qp.jpg");
+
+        for (Map.Entry<String, String> entry : draws.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            TblDraw tblDraw = new TblDraw();
+            tblDraw.setTerm(key);
+            tblDraw.setUrl(value);
+            tblDraw.setUserId("-1");
+            defaultDraws.add(tblDraw);
+        }
     }
+
+    public static List<TblQuestion> defaultQuestions = new ArrayList<>();
+
+    public static List<TblDraw> defaultDraws = new ArrayList<>();
 
     @Autowired
     private ChatGptService chatGptService;
@@ -57,13 +112,15 @@ public class ContentController {
     @Autowired
     private TblDrawService tblDrawService;
 
-    @Autowired
-    private TblDefaultQuestionService tblDefaultQuestionService;
-
     @RequestMapping("/defaultQuestions")
     public Response<List<TblQuestion>> getDefaultQuestions() {
         try {
-            return Response.success(tblDefaultQuestionService.queryRandom());
+            List<TblQuestion> res = new ArrayList<>();
+            Collections.shuffle(defaultQuestions);
+            for (int i = 0; i < 10; i++) {
+                res.add(defaultQuestions.get(i));
+            }
+            return Response.success(res);
         } catch (Throwable t) {
             log.error("getDefaultQuestions exception, t:", t);
             return Response.fail(t.getMessage());
@@ -73,7 +130,12 @@ public class ContentController {
     @RequestMapping("/defaultDraws")
     public Response<List<TblDraw>> getDefaultDraws() {
         try {
-            return Response.success(tblDrawService.queryRandom());
+            List<TblDraw> res = new ArrayList<>();
+            Collections.shuffle(defaultDraws);
+            for (int i = 0; i < 4; i++) {
+                res.add(defaultDraws.get(i));
+            }
+            return Response.success(res);
         } catch (Throwable t) {
             log.error("getDefaultDraws exception, t:", t);
             return Response.fail(t.getMessage());
