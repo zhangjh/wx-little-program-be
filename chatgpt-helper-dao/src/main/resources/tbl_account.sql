67acdf72d5fe22dc14c48b32068b372e5df3c045
zhangjh451 <zhangjh451@midea.com>
2023-02-17 17:30:19 +0800

    不记录小程序用户昵称

--- chatgpt-helper-dao/src/main/resources/tbl_account.sql
+++ chatgpt-helper-dao/src/main/resources/tbl_account.sql
@@ -7,9 +7,9 @@ CREATE TABLE tbl_account
     `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
     `modify_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
     `is_deleted`  TINYINT(1) DEFAULT 0  NOT NULL COMMENT '逻辑删除',
-    `nick_name`   VARCHAR(64) NOT NULL COMMENT '昵称',
-    `avatar`      VARCHAR(255) NOT NULL COMMENT '头像',
-    `ext_id`       VARCHAR(64) NOT NULL COMMENT '外部系统id',
+    `nick_name`   VARCHAR(64) NULL COMMENT '昵称',
+    `avatar`      VARCHAR(255) NULL COMMENT '头像',
+    `ext_id`       VARCHAR(64) NULL COMMENT '外部系统id',
     `ext_type`     TINYINT(1) NOT NULL COMMENT '外部系统类型',
     `mobile`      VARCHAR(11) NULL COMMENT '手机',
     PRIMARY KEY (id)
